a4669c43215e4cff558b279863cac19639835e74
Johnson Li <jli39@stuy.edu>
2019-02-03 16:14:04 -0500

    a

--- 02_canvas/dotconn.js
+++ 02_canvas/dotconn.js
@@ -7,31 +7,26 @@ const c = document.getElementById('playground');
 const clear = document.getElementById('clear');
 
 var ctx = c.getContext('2d');
-var prevX;
-var prevY;
+
 // eventlistener for clear button
 clear.addEventListener('click', () => {
     // console.log('clearing');
     ctx.clearRect(0, 0, c.width, c.height);
-    prevX = null;
-    prevY = null;
     ctx.beginPath();
 });
 
 // eventlistener for a click on the canvas
 c.addEventListener('click', (e) => {
     // console.log(e.offsetX, e.offsetY);
-    ctx.beginPath();
-    ctx.moveTo(e.offsetX, e.offsetY);
-
-    if(prevX && prevY){
-        ctx.lineTo(prevX, prevY);
-        ctx.closePath();
-    }
-    prevX = e.offsetX;
-    prevY = e.offsetY;
-    ctx.fillStyle = 'aqua';
-    // e.offsetX, e.offsetY, correct coordinates without getBoundingClientRect()
-    ctx.arc(e.offsetX, e.offsetY, 50, 0, 2 * Math.PI);
+    let currX = e.offsetX;
+    let currY = e.offsetY;
+    ctx.lineTo(currX, currY);
     ctx.stroke();
+
+    ctx.beginPath()
+    ctx.arc(currX, currY, 50, 0, 2 * Math.PI);
+    ctx.stroke();
+
+    ctx.beginPath();
+    ctx.moveTo(currX, currY);
 });
