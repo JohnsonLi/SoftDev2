8659ed4d119b3db3c22868be7662166a65f83afc
Johnson Li <jli39@stuy.edu>
2019-03-07 18:22:29 -0500

    fixed ip thing and added extra check for year

--- 08_mongosite/app.py
+++ 08_mongosite/app.py
@@ -5,12 +5,11 @@
 
 import os
 
-from flask import Flask, render_template, request, flash, redirect, url_for
+from flask import Flask, render_template, request, flash, redirect, url_for, session
 
 import mongo
        
 app = Flask(__name__)
-SERVER_ADDR = '206.81.0.191'
 
 app.secret_key = os.urandom(32)
 
@@ -23,10 +22,10 @@ def connect():
     ip = request.args['ip'].strip()
     if ip:
         print('IP SET')
-        SERVER_ADDR = ip
+        session['ip'] = ip
 
         try:
-            mongo.import_data(SERVER_ADDR, 'chinagdp.json')
+            mongo.import_data(session['ip'], 'chinagdp.json')
             return redirect(url_for('options'))
         except:
             flash('enter a valid ip please')
@@ -49,13 +48,18 @@ def result():
             flash('enter a valid year please')
             return redirect(url_for('options'))
 
-        data = mongo.year_search(SERVER_ADDR, year)
-        return '<h3> GDP of China in {} was ${} </h3>'.format(year,data[0]['value'])
+        data = mongo.year_search(session['ip'], year)
+        return '<a href="http://localhost:5000/options"> Back </a> <br> <h3> GDP of China in {} was ${} </h3>'.format(year,data[0]['value'])
 
     if request.args['option'] == 'between':
         year0 = request.args['year0']
         year1 = request.args['year1']
-        data = mongo.yearbtwn_search(SERVER_ADDR, year0, year1)
+
+        if int(year0) > 2017 or int(year0) < 1960 or int(year1) > 2017 or int(year1) < 1960:
+            flash('enter a valid year please')
+            return redirect(url_for('options'))
+
+        data = mongo.yearbtwn_search(session['ip'], year0, year1)
         parsed = []
         for item in data:
             parsed.append({'year': item['date'], 'gdp': item['value']})
@@ -63,9 +67,9 @@ def result():
 
     if request.args['option'] == 'abovebelow':
         if request.args['abovebelow'] == 'above':
-            data = mongo.gdp_search(SERVER_ADDR, request.args['value'], 1)
+            data = mongo.gdp_search(session['ip'], request.args['value'], 1)
         else:
-            data = mongo.gdp_search(SERVER_ADDR, request.args['value'], 0)
+            data = mongo.gdp_search(session['ip'], request.args['value'], 0)
         parsed = []
         for item in data:
             parsed.append({'year': item['date'], 'gdp': item['value']})
